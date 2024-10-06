dd067c3f2fb33df72d85c090def34c5fc07db023
Ayush tomar <ayushtomar388@gmail.com>
2024-10-06 22:22:23 +0530

    Section Created

---
+++ src/components/WorkSection.tsx
@@ -0,0 +1,44 @@
+import SectionHeading from "./SectionHeading";
+
+const WorkSection = () => {
+    return ( 
+        <div>
+            <SectionHeading heading="OUR WORK"/>
+            <div className="my-8 mb-20">
+                <div className="flex gap-5 justify-center mb-10">
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                    <div className="h-20 w-20 bg-gray-600">
+
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 gap-5 w-[80vw] mx-auto">
+                    <div className="h-60 bg-slate-800">
+
+                    </div>
+                    <div className="h-60 bg-slate-800">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default WorkSection;
