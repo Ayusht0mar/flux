ad6046314a10cef0c9416d4c4ca4468aa5277b2f
Ayush tomar <ayushtomar388@gmail.com>
2024-10-30 11:54:50 +0530

    Image added and Text size changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 const Header = () => {
     return ( 
         <header className="flex justify-between w-[90vw] mx-auto items-center py-6 bg-black text-white">
-            <div className="text-2xl font-bold w-full">FLUX</div>
+            <div className="w-full">
+                <Image
+                    src="/logo.svg"
+                    alt="logo"
+                    width={32}
+                    height={48}
+                />
+            </div>
             <nav>
-            <ul className="flex space-x-6">
+            <ul className="flex space-x-6 text-xl tracking-wide">
                 <li><a href="/">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Services</a></li>
@@ -12,7 +21,7 @@ const Header = () => {
             </ul>
             </nav>
             <div className="w-full flex justify-end">
-                <button className="px-9 py-2  border border-orange-500 hover:bg-orange-600 text-white rounded-full">Schedule a meet</button>
+                <button className="px-9 py-2 text-lg border border-orange-500 hover:bg-orange-600 text-white rounded-full">Schedule a meet</button>
             </div>
         </header>
      );
