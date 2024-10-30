dac124553ab73e22c1da04eda48ce8586cb30831
Ayush tomar <ayushtomar388@gmail.com>
2024-10-30 11:57:36 +0530

    Styled

--- src/components/HeroArea.tsx
+++ src/components/HeroArea.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 const HeroArea = () => {
     return ( 
         <section className="relative mx-auto w-[90vw] bg-black text-white py-16">
-            <div className="grid grid-cols-2">
-                <h1 className="text-5xl leading-normal uppercase font-bold text-[#E5E5E5]">Translating Your Thoughts and Visions</h1>
+            <div className="grid items-center grid-cols-3">
+                <h1 className="col-span-2 text-[80px] leading-none uppercase font-bold text-[#E5E5E5] font-barlowCondensed">Translating Your <br /> Thoughts and Visions</h1>
                 <div>
-                    <p className="mt-4 text-lg">
+                    <p className="mt-4 text-xl tracking-wide">
                     We Provide Our Art To Ensure That Human Insights Work Well Together With Technology, Brand, and Business, With a Focus on Fast and Assertive Results.
                     </p>
                     <div className="flex mt-8">
-                    <button className="px-6 py-2 bg-[#FE752D] hover:bg-orange-600 text-[#0d0d0d] font-semibold rounded-full mr-4">Schedule a meet</button>
-                    <button className="px-6 py-2 border border-[#FE752D] text-white rounded-full">Our Work</button>
+                    <button className="px-6 py-2 bg-[#FE752D] hover:bg-orange-600 text-lg text-[#0d0d0d] font-semibold rounded-full mr-4">Schedule a meet</button>
+                    <button className="px-6 py-2 text-lg border border-[#FE752D] text-white rounded-full">Our Work</button>
                     </div>
                 </div>
             </div>
