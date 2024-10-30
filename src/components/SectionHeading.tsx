3d7d326786e1c95970843af06509c9b650a35ffc
Ayush tomar <ayushtomar388@gmail.com>
2024-10-30 11:59:13 +0530

    Stlyed

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -6,12 +6,12 @@ const SectionHeading: React.FC<SectionHeadingProps> = ({
     heading
 }) => {
     return ( 
-        <div className="border-y border-[#8E98A8] py-4 flex gap-8 justify-center w-full overflow-hidden">
-            <h3 className="text-5xl font-bold text-[#E5E5E525]">{heading}</h3>
-            <h3 className="text-5xl font-bold text-[#E5E5E525]">{heading}</h3>
-            <h3 className="text-5xl font-bold text-[#E5E5E575]">{heading}</h3>
-            <h3 className="text-5xl font-bold text-[#E5E5E525]">{heading}</h3>
-            <h3 className="text-5xl font-bold text-[#E5E5E525]">{heading}</h3>
+        <div className="text-6xl font-bold border-y border-[#8E98A8] py-3 flex gap-8 justify-center w-full font-barlowCondensed overflow-hidden">
+            <h3 className="text-[#E5E5E525]">{heading}</h3>
+            <h3 className="text-[#E5E5E525]">{heading}</h3>
+            <h3 className="text-[#E5E5E575]">{heading}</h3>
+            <h3 className="text-[#E5E5E525]">{heading}</h3>
+            <h3 className="text-[#E5E5E525]">{heading}</h3>
         </div>
      );
 }
