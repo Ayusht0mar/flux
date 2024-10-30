4ecb2406738cdcd61ad53861e9be2a4e9dca150f
Ayush tomar <ayushtomar388@gmail.com>
2024-10-30 11:59:29 +0530

    Logos added

--- src/components/WorkSection.tsx
+++ src/components/WorkSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SectionHeading from "./SectionHeading";
 
 const WorkSection = () => {
@@ -6,33 +7,48 @@ const WorkSection = () => {
             <SectionHeading heading="OUR WORK"/>
             <div className="my-8 mb-20">
                 <div className="flex gap-5 justify-center mb-10">
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
-                    <div className="h-20 w-20 bg-gray-600">
-
-                    </div>
+                    <Image
+                        src="/logos/1.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
+                    <Image
+                        src="/logos/2.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
+                    <Image
+                        src="/logos/3.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
+                    <Image
+                        src="/logos/4.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
+                    <Image
+                        src="/logos/5.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
+                    <Image
+                        src="/logos/6.svg"
+                        alt=""
+                        width={80}
+                        height={48}
+                    />
                 </div>
                 <div className="grid grid-cols-2 gap-5 w-[80vw] mx-auto">
-                    <div className="h-60 bg-slate-800">
+                    <div className="h-80 rounded-lg bg-slate-800">
 
                     </div>
-                    <div className="h-60 bg-slate-800">
+                    <div className="h-80 rounded-lg bg-slate-800">
 
                     </div>
                 </div>
